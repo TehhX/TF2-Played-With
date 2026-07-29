6821d459205fb35dcd712bf945b39a476e1644e0
TehhX <samuel.tobias@tehx.ca>
2026-07-29 14:07:51 -0400

    Implemented change from gh/TehhX/Learning-C/Assorted/sorted_insert.c

--- src/sorted_find_insert.c
+++ src/sorted_find_insert.c
@@ -1,9 +1,7 @@
-#include "common.h"
 #include "sorted_find_insert.h"
 
 #include "string.h"
 
-// IMMED_TODO: Use compare(...) function once, store value to test again
 void sorted_find_insert(const void *const value, void **const array, size_t *const array_len, const size_t element_size, sorted_find_insert_action_t on_find, sorted_find_insert_action_t on_insert, void *const params, const __compar_fn_t compare)
 {
     if (*array_len == 0)
@@ -15,14 +13,14 @@ void sorted_find_insert(const void *const value, void **const array, size_t *con
     }
     else if (compare(*array, value) > 0)
     {
-        PREALLOCS(*array, ++*array_len, element_size);
+        *array = realloc(*array, element_size * ++*array_len);
         memmove(*array + element_size, *array, element_size * (*array_len - 1));
         on_insert(*array, params);
         return;
     }
     else if (compare(*array + element_size * (*array_len - 1), value) < 0)
     {
-        PREALLOCS(*array, ++*array_len, element_size);
+        *array = realloc(*array, element_size * ++*array_len);
         on_insert(*array + element_size * (*array_len - 1), params);
         return;
     }
@@ -31,16 +29,18 @@ void sorted_find_insert(const void *const value, void **const array, size_t *con
     while (low < high)
     {
         const size_t mid = low + (high - low) / 2;
-        if (compare(*array + element_size * mid, value) == 0)
+        const int compare_result = compare(*array + element_size * mid, value);
+
+        if (compare_result == 0)
         {
             on_find(*array + element_size * mid, params);
             return;
         }
-        else if (compare(*array + element_size * mid, value) < 0)
+        else if (compare_result < 0)
         {
             low = mid + 1;
         }
-        else // Implicitly compare(*array + element_size * mid, value) > 0
+        else // Implicitly compare_result > 0
         {
             high = mid - 1;
         }
@@ -51,10 +51,13 @@ void sorted_find_insert(const void *const value, void **const array, size_t *con
         on_find(*array + element_size * low, params);
         return;
     }
-    else
+
+    if (compare(value, *array + element_size * low) > 0)
     {
-        PREALLOCS(*array, ++*array_len, element_size);
-        memmove(*array + element_size * (low + 1), *array + element_size * low, element_size * (*array_len - low - 1));
-        on_insert(*array + element_size * low, params);
+        ++low;
     }
+
+    *array = realloc(*array, element_size * ++*array_len);
+    memmove(*array + element_size * (low + 1), *array + element_size * low, element_size * (*array_len - low - 1));
+    on_insert(*array + element_size * low, params);
 }
